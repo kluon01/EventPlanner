e1427e2d41af1a379c48ab445458739a0cbeac1e
kluon01 <karshinl01@gmail.com>
2019-12-11 14:11:33 -0600

    fixed add and edit event pages pt2

--- app/src/main/java/com/example/eventplanner/ui/EditEventActivity.java
+++ app/src/main/java/com/example/eventplanner/ui/EditEventActivity.java
@@ -1,5 +1,7 @@
 package com.example.eventplanner.ui;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import com.google.android.material.checkbox.MaterialCheckBox;
@@ -13,9 +15,14 @@ import butterknife.ButterKnife;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.example.eventplanner.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class EditEventActivity extends AppCompatActivity {
 
@@ -23,8 +30,18 @@ public class EditEventActivity extends AppCompatActivity {
     EditText titleInput;
     @BindView(R.id.edit_subtitle)
     EditText subtitleInput;
-    @BindView(R.id.edit_address)
-    EditText addressInput;
+    @BindView(R.id.edit_time)
+    EditText timeInput;
+    @BindView(R.id.edit_date)
+    EditText dateInput;
+    @BindView(R.id.edit_street)
+    EditText streetInput;
+    @BindView(R.id.edit_city)
+    EditText cityInput;
+    @BindView(R.id.edit_state)
+    EditText stateInput;
+    @BindView(R.id.edit_zipcode)
+    EditText zipcodeInput;
     @BindView(R.id.edit_info)
     EditText infoInput;
     @BindView(R.id.edit_event_button)
@@ -32,11 +49,54 @@ public class EditEventActivity extends AppCompatActivity {
     @BindView(R.id.public_check_edit)
     MaterialCheckBox publicCheckbox;
 
+    private Calendar myCalendar;
+    private TimePickerDialog timePickerDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_event);
         ButterKnife.bind(this);
+
+        myCalendar = Calendar.getInstance();
+
+        // Open time picker
+        timeInput.setOnClickListener(view -> {
+            timePickerDialog = new TimePickerDialog(EditEventActivity.this, (timePicker, hourOfDay, minutes) -> {
+                boolean amPm;
+                if (hourOfDay >= 12) {
+                    amPm = true;
+                } else {
+                    amPm = false;
+                }
+                timeInput.setText(String.format("%02d:%02d %s", (hourOfDay == 12 || hourOfDay == 0) ? 12 : hourOfDay % 12, minutes, amPm ? "PM" : "AM"));
+            }, 0, 0, false);
+            timePickerDialog.show();
+        });
+
+        // Open date picker when user taps on date field
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy", Locale.US);
+                dateInput.setText(sdf.format(myCalendar.getTime()));
+            }
+
+        };
+
+        // Set date text
+        dateInput.setOnClickListener(v -> {
+            // TODO Auto-generated method stub
+            new DatePickerDialog(EditEventActivity.this, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        });
     }
 
 }
